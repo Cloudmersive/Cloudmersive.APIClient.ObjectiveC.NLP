37eb885fb4cf5806ae35ae542906df6255dfb968
Cloudmersive <35204726+Cloudmersive@users.noreply.github.com>
2019-12-22 15:40:24 -0800

    Upgraded to NLP v2 API

--- build.ps1
+++ build.ps1
@@ -1,4 +1,4 @@
 ﻿Remove-Item –path ./client –recurse
-& java -jar swagger-codegen-cli.jar generate -i https://api.cloudmersive.com/swagger/api/nlp -l objc -o client -c packageconfig.json
+& java -jar swagger-codegen-cli.jar generate -i https://api.cloudmersive.com/swagger/api/nlpv2 -l objc -o client -c packageconfig.json
 (Get-Content ./client/CloudmersiveNLPApiClient.podspec).replace('CloudmersiveNLPApiClient/**/*.{m,h}', "client/CloudmersiveNLPApiClient/**/*.{m,h}") | Set-Content ./client/CloudmersiveNLPApiClient.podspec
 (Get-Content ./client/CloudmersiveNLPApiClient.podspec).replace('CloudmersiveNLPApiClient/**/*.h', "client/CloudmersiveNLPApiClient/**/*.h") | Set-Content ./client/CloudmersiveNLPApiClient.podspec
